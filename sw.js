662bf53f3cde8dbd373d0df5f06f9550b1b3e2b8
Jetti Hrushikesh <onlybugs05@gmail.com>
2026-05-11 21:29:50 +0530

    Update service worker options and domain

--- sw.js
+++ sw.js
@@ -4,3 +4,12 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10993297
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
